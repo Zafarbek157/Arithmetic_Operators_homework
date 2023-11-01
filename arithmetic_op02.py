048965606e40aaf72333ed7b5fd10184fdeacd85
Zafarbek157 <zafarbek.saidaxmadov2811@gmail.com>
2023-11-01 08:40:04 +0500

    added solutions

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,6 +1,6 @@
 #Create a varaible 'number' and assign it the value of 43.
-raqam=43
+number=43
 #Create a variable 'answer' and assign it the sum of the number's digits.
-raqamlariyigindisi=raqam%10+raqam//10
+raqamlariyigindisi=number%10+number//10
 #Print answer.
 print(raqamlariyigindisi)
--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,6 +1,6 @@
 #Create a variable called 'number' and assign it the two-digit number
-raqam=int(input("2 xonali sonni kiriting"))
+number=int(input("2 xonali sonni kiriting"))
 #Find the reverse of the number and assign it to a variable called 'answer'.
-javob=(raqam%10)*10+raqam//10
+answer=(number%10)*10+number//10
 #Print the answer variable
-print(javob)
+print(answer)
